f252a25d04922b95f931eec1668106d119e45a41
Hugo0133 <hugojimenezgarcia12345@gmail.com>
2021-08-07 13:10:17 +0200

    Documentada clase ICabeza

--- Snake/src/main/java/Juegos/Snake/ICabeza.java
+++ Snake/src/main/java/Juegos/Snake/ICabeza.java
@@ -2,9 +2,19 @@ package Juegos.Snake;
 
 //@@ Project : Snake
 //@@ File Name : ICabeza
-//@@ Author :  Hugo
+//@@ Author :  Hugo0133
 
 
+/**
+ * Interfaz de la clase Cabeza para su utilizacion por parte de la clase Tablero.
+ * @author Hugo0133
+ */
 public interface ICabeza{
+	/**
+	 * Devuelve true si hay algun elemento de la serpiente en la posicion pasada como parametro.
+	 * @param fila Fila en la que realizar la comprobacion.
+	 * @param columna Columna en la que realizar la comprobacion.
+	 * @return True si hay algun elemento de la serpiente en la posicion pasada como parametro.
+	 */
 	public boolean comprobarPosicionOcupada(int fila, int columna);
 }
