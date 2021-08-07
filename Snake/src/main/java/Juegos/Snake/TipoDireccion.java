06047ad1d86a7fab072443c85982bffa0d78cfa7
Hugo0133 <hugojimenezgarcia12345@gmail.com>
2021-08-07 13:18:50 +0200

    Documentado enumeracion TipoDireccion

--- Snake/src/main/java/Juegos/Snake/TipoDireccion.java
+++ Snake/src/main/java/Juegos/Snake/TipoDireccion.java
@@ -2,8 +2,14 @@ package Juegos.Snake;
 
 //@@ Project : Snake
 //@@ File Name : TipoDireccion
-//@@ Author :  Hugo
+//@@ Author :  Hugo0133
 
+
+/**
+ * Establece las direcciones disponibles en este juego, es decir, arriba, abajo, derecha e izquierda.
+ * @author Hugo0133
+ *
+ */
 public enum TipoDireccion {
 	ARRIBA,
 	DERECHA,
