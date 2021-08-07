34ac9709c06c83f9b3522aa3d7d292355669dcf6
Hugo0133 <hugojimenezgarcia12345@gmail.com>
2021-08-07 13:01:33 +0200

    Documentacion generada

---
+++ Snake/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"Juegos.Snake","l":"Cabeza"},{"p":"Juegos.Snake","l":"Cola"},{"p":"Juegos.Snake","l":"Fruto"},{"p":"Juegos.Snake","l":"ICabeza"},{"p":"Juegos.Snake","l":"JuegoSnake"},{"p":"Juegos.Snake","l":"Tablero"},{"p":"Juegos.Snake","l":"TipoDireccion"}];updateSearchResults();
